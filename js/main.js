333ac1f4ed0a0f28256a6cba03962d8fad80c626
haidernawaz <133153122+haidernawaz@users.noreply.github.com>
2024-05-22 23:49:31 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+   setTimeout(function() {
+        var popups = document.getElementsByClassName("popup");
+        for (var i = 0; i < popups.length; i++) {
+            popups[i].style.display = "none";
+        }
+    }, 3000);
